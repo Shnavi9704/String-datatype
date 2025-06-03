d08dce20c4cdafc643fc930cdca013ab4f4c05d6
Shnavi9704 <chavanvishu622@gmail.com>
2025-06-03 22:06:10 +0530

    Add files via upload

---
+++ complex_datatype2.py
@@ -0,0 +1,20 @@
+S = "INSTAGRAM"
+print (S[5]) # positive value          o/p=G
+print (S[-4]) # Negative value         o/p=G  
+
+print (S[6:9]) #positive value         o/p= RAM
+print (S[-3: ]) # Negative value       o/p= RAM
+
+print(S[6:len(S)]) # positive value    o/p= RAM
+print(S[-3:len(S)]) ,# Negative value  o/p= RAM 
+
+print(S[9:0:-1]) #, positive value     o/p= MARGATSN
+print(S[ : :-1]) # Negative value      o/p= MARGATSNI
+
+print(S[0:9:2])  #, positive value     o/p= ISARM
+print(S[ : :-2]) # Negative value      o/p= MRASI
+
+print(S[6: ]), # positive value        o/p= RAM
+print(S[-3: ])# Negative value         o/p= RAM
+
+
